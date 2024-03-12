7077686ddcf5c7385fec2cfa69130f1ab0034225
V92 <valjek92@gmail.com>
2024-03-12 23:55:11 +0100

    Refactoring of the bot code, game structure added

--- src/main/java/ua/javarush/MyFirstTelegramBot.java
+++ src/main/java/ua/javarush/MyFirstTelegramBot.java
@@ -14,72 +14,136 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import com.google.common.base.Utf8;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import static ua.javarush.TelegramBotContent.*;
 import static ua.javarush.TelegramBotUtils.*;
-import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.Dotenv;  //Setting the opportunity to use .env files  
 
 public class MyFirstTelegramBot extends TelegramLongPollingBot {
     private final Map<Long,String> chatIdToUserName=new HashMap<>();
-    private final Set<Long> awaitingName = new HashSet<>(); // Для отслеживания, кто должен ввести имя
+    private final Set<Long> awaitingName = new HashSet<>(); // For tracking of who must to enter a name
+    //For increased bot variability we're using the list of popular greetings now 
+    List<String> greetings = Arrays.asList("привет", "прив", "хай", "ку", "привiт", "вiтаю", "cześć","hello", "hi", "hey", "hola", "bonjour", "ciao", "hallo");
        
     @Override
     public String getBotUsername() {
-        // TODO: додай ім'я бота в лапки нижче 
+        // додай ім'я бота в лапки нижче 
         Dotenv dotenv = Dotenv.load();       
-        String botUsername = dotenv.get("BOT_USRNAME");        
+        String botUsername = dotenv.get("BOT_USRNAME"); //For increased safety we're not using the direct bot username anymore      
         return botUsername;
     }
 
     @Override
     public String getBotToken() {
-        // TODO: додай токен бота в лапки нижче
+        // додай токен бота в лапки нижче
         Dotenv dotenv = Dotenv.load();
-        String botToken = dotenv.get("BOT_TOKEN");        
+        String botToken = dotenv.get("BOT_TOKEN");  //For increased safety we're not using the direct bot token anymore        
         return botToken;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        // TODO: основний функціонал бота будемо писати тут
-        Long ChatId = getChatId(update);
-        //Checking, is the text message exists?
-        if (!update.hasMessage()||!update.getMessage().hasText()) return;
-        String incomingText = update.getMessage().getText();
+        // основний функціонал бота будемо писати тут
+        Long ChatId = getChatId(update);              
         SendMessage message = new SendMessage(); 
         message.enableHtml(true); // Setting HTML formatting
-        message.setChatId(String.valueOf(ChatId));
-
-        // Processing command /start
-    if (incomingText.equals("/start")) {
-        message.setText("Привiт, <i>майбутнiй</i> програмiст!");        
-    } else if (incomingText.contains("привiт")) {        
-        message.setText("Як тебе звуть?"); 
-        awaitingName.add(ChatId); // We note that we're waiting for the username       
-    } else if (awaitingName.contains(ChatId)) {
-        // The user enters his name
-        chatIdToUserName.put(ChatId, incomingText); // Saving the username
-        message.setText("Радий знайомству, " + incomingText + ", я - <b>Кiт</b>");
-        awaitingName.remove(ChatId); // Removing the wait mark
-    } else {
-        // For any other message
-        message.setText("Радий тебе бачити знову, " + chatIdToUserName.getOrDefault(ChatId, "незнайомець") + "!");
+        //Checking, is the text message exists?  
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String incomingText = update.getMessage().getText().toLowerCase();
+            message.setChatId(String.valueOf(ChatId));
+        // Processing command /start    
+        String lowerCaseText = incomingText.toLowerCase(); //for the code optimization now we're setting .toLowerCase before the statement below
+        // Now instead incomingText.contains("привiт") we're using modified checking
+        boolean isGreeting = greetings.stream().anyMatch(greet -> lowerCaseText.contains(greet));
+        if ("/start".equals(lowerCaseText)) {
+            message.setText("Привiт, <i>майбутнiй</i> програмiст!");        
+        } else if (isGreeting) {        
+            message.setText("Як тебе звуть?"); 
+            awaitingName.add(ChatId); // We note that we're waiting for the username       
+        } else if (awaitingName.contains(ChatId)) {
+            addGlories(ChatId, 0);
+            // The user enters his name
+            chatIdToUserName.put(ChatId, incomingText); // Saving the username
+            //message.setText("Радий знайомству, " + incomingText + "!");            
+            awaitingName.remove(ChatId); // Removing the wait mark
+            SendMessage message2 = createMessage(ChatId, STEP_1_TEXT, Map.of("Злам холодильника +20 слави","step_1_btn"));
+            sendApiMethodAsync(message2);
+        } 
+        sendApiMethodAsync(message);
     }
 
-        try {
-            execute(message); //Sending the message
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+    if (update.hasCallbackQuery()) {
+        ChatId = update.getCallbackQuery().getMessage().getChatId(); //Checking
+        if (update.getCallbackQuery().getData().equals("step_1_btn") && getGlories(ChatId) == 0) {
+            addGlories(ChatId, 20);
+            SendMessage message2 = createMessage(ChatId, STEP_2_TEXT, Map.of(
+                "Взяти сосиску! +20 слави","step_2_btn",
+                "Взяти рибку! +20 слави","step_2_btn",
+                "Скинути банку з огірками! +20 слави","step_2_btn"));
+            sendApiMethodAsync(message2);
         }
-        
-    }
+        if (update.getCallbackQuery().getData().equals("step_2_btn") && getGlories(ChatId) == 20) {
+            addGlories(ChatId, 20);
+            SendMessage message2 = createMessage(ChatId, STEP_3_TEXT, Map.of(
+                "Злам робота пилососа +30 слави","step_3_btn"));
+            sendApiMethodAsync(message2);
+        }
+        if (update.getCallbackQuery().getData().equals("step_3_btn") && getGlories(ChatId) == 40) {
+            addGlories(ChatId, 30);
+            SendMessage message2 = createMessage(ChatId, STEP_4_TEXT, Map.of(
+                "Відправити робопилосос за їжею! +30 слави","step_4_btn",
+                "Проїхатися на робопилососі! +30 слави", "step_4_btn",
+                "Тікати від робопилососа! +30 слави","step_4_btn"));
+            sendApiMethodAsync(message2);
+        }
+        if (update.getCallbackQuery().getData().equals("step_4_btn") && getGlories(ChatId) == 70) {
+            addGlories(ChatId, 30);
+            SendMessage message2 = createMessage(ChatId, STEP_5_TEXT, Map.of(
+                "Одягнути та включити GoPro! +40 слави","step_5_btn"));
+            sendApiMethodAsync(message2);
+        }
+        if (update.getCallbackQuery().getData().equals("step_5_btn") && getGlories(ChatId) == 100) {
+            addGlories(ChatId, 40);
+            SendMessage message2 = createMessage(ChatId, STEP_6_TEXT, Map.of(
+                "Бігати дахами, знімати на GoPro! +40 слави","step_6_btn",
+                "З GoPro нападати на інших котів із засідки! +40 слави", "step_6_btn",
+                "З GoPro нападати на собак із засідки! +40 слави","step_6_btn"));
+            sendApiMethodAsync(message2);
+        }
+        if (update.getCallbackQuery().getData().equals("step_6_btn") && getGlories(ChatId) == 140) {
+            addGlories(ChatId, 40);
+            SendMessage message2 = createMessage(ChatId, STEP_7_TEXT, Map.of(
+                "Злам пароля","step_7_btn"));
+            sendApiMethodAsync(message2);
+        }
+        if (update.getCallbackQuery().getData().equals("step_7_btn") && getGlories(ChatId) == 180) {
+            addGlories(ChatId, 50);
+            SendMessage message2 = createMessage(ChatId, STEP_8_TEXT, Map.of(
+                "Залити вiдео на комп'ютер +50 слави","step_8_btn"));                
+            sendApiMethodAsync(message2);
+        }
+        if (update.getCallbackQuery().getData().equals("step_8_btn") && getGlories(ChatId) == 230) {
+            addGlories(ChatId, 50);
+            SendMessage message2 = createMessage(ChatId, FINAL_TEXT, Map.of(
+                "Вийти на подвір'я","final_btn"));                
+            sendApiMethodAsync(message2);
+        }
+    }        
+}
 
     private Long getChatId(Update update) {
-        return update.getMessage().getChatId();
+        if (update.hasMessage()) {
+            return update.getMessage().getChatId();
+        } else if (update.hasCallbackQuery()) {
+            return update.getCallbackQuery().getMessage().getChatId();
+        }
+        return null; // или какая-то логика обработки неопределенного состояния
     }
 
     public static void main(String[] args) throws TelegramApiException {
